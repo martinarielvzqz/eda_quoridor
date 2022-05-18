f004b6c40c2e746e616375a24a2145456bbb9339
Martin Vazquez <martin@linisco.com.ar>
2022-05-18 17:36:08 -0300

    fix test

--- tests/test_quoridor.py
+++ tests/test_quoridor.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from quoridor.constants import NORTH_PAWN
+from quoridor.constants import CELL_NORTH_PAWN
 from quoridor.quoridor import Quoridor, QuoridorException
 
 
@@ -44,12 +44,12 @@ class TestQuoridor(TestCase):
         assert isinstance(game, Quoridor)
         assert (
             game.player == self.player_name_1
-            if game.side == NORTH_PAWN
+            if game.side == CELL_NORTH_PAWN
             else self.player_name_2
         )
         assert (
             game.opponent == self.player_name_2
-            if game.side == NORTH_PAWN
+            if game.side == CELL_NORTH_PAWN
             else self.player_name_1
         )
 
